5582925ee02b1dff66f5f903c3bf59984adf56e3
orgesified <gesishabani1@gmail.com>
2020-08-23 23:17:10 +0200

    import new ContextCompat from androidx
    Signed-off-by: orgesified <gesishabani1@gmail.com>

--- app/src/main/java/com/ruesga/rview/MainActivity.java
+++ app/src/main/java/com/ruesga/rview/MainActivity.java
@@ -40,6 +40,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
